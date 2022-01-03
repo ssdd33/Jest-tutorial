9b2aba47ad4454f5f0c6048aa82c8d26d3826f28
LAPTOP-ICBL9CQ4\비플러스랩 <beeblu.0@gmail.com>
2022-01-04 08:59:15 +0900

    test async

--- src/test/async.test.js
+++ src/test/async.test.js
@@ -1,12 +1,36 @@
 const fetchProduct = require("../async");
 
-test("rejected", () => {
-  expect.assertions(1);
-  return fetchProduct("error").catch((e) => expect(e).toMatch("network error"));
+describe("Async", () => {
+  it("async-done", (done) => {
+    fetchProduct().then((data) => {
+      expect(data).toEqual({ item: "milk", price: 2000 });
+      done();
+    });
+  });
+
+  it("async-return", () => {
+    return fetchProduct().then((data) => {
+      expect(data).toEqual({ item: "milk", price: 2000 });
+    });
+  });
+
+  it("async-await", async () => {
+    const product = await fetchProduct();
+    expect(product).toEqual({ item: "milk", price: 2000 });
+  });
+
+  it("async-resolves", () => {
+    return expect(fetchProduct()).resolves.toEqual({
+      item: "milk",
+      price: 2000,
+    });
+  });
+
+  it("async-reject", () => {
+    return expect(fetchProduct("error")).rejects.toBe("network error");
+  });
 });
 
-test("resolved", () => {
-  return fetchProduct().then((data) => {
-    expect(data.item).toBe("milk");
-  });
+test("rejected", () => {
+  return fetchProduct("error").catch((e) => expect(e).toMatch("network error"));
 });
