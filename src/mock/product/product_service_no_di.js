cd40cfa2714753bdab26b9524fcdb3816f23f346
LAPTOP-ICBL9CQ4\비플러스랩 <beeblu.0@gmail.com>
2022-01-05 09:47:46 +0900

    add comment about bad case

--- src/mock/product/product_service_no_di.js
+++ src/mock/product/product_service_no_di.js
@@ -3,7 +3,9 @@ const ProductClient = require("./product_client");
 
 class ProductService {
   constructor() {
-    /* 테스트 코드의 독립성이 지켜지지 않는다 :  
+    /*
+    Bad case 1. class 내부에서 스스로의 의존을 정의하는 것은 dependency injection 원칙에 어긋난다.   
+    테스트 코드의 독립성이 지켜지지 않는다 :  
       productService를 테스트하면서 productClient 내부의 fetch함수도 함께 테스트 된다.
       네트워크나,  fetch함수 로직의 문제로 productService의 테스트가 실패할 수도 있음.
       -> productClient를 mock함으로써 productService의 독립성을 지킬 수 있다. */
